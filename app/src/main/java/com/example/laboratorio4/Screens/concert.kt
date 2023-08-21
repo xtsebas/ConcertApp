a1d90baa72def76cedbe68211bf0cbb4751ac305
Sebastian <thiagohugo2014@gmail.com>
2023-08-21 10:10:06 -0600

    Creada las cartas de presentacion de conciertos, falta mejoras

--- app/src/main/java/com/example/laboratorio4/Screens/concert.kt
+++ app/src/main/java/com/example/laboratorio4/Screens/concert.kt
@@ -1,14 +1,19 @@
 package com.example.laboratorio4.Screens
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -22,75 +27,80 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.laboratorio4.R
+import com.example.laboratorio4.login
 
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            concert()
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun concert() {
-    Column(modifier = Modifier.padding(20.dp)) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            ConcertCard(
-                imageId = R.drawable.wos,
-                artistName = "Wos"
-            )
-            Spacer(modifier = Modifier.width(16.dp))
-            ConcertCard(
-                imageId = R.drawable.molotov,
-                artistName = "Molotov"
-            )
-        }
+    Column(
+        modifier = Modifier
+            .fillMaxSize() // Para que ocupe todo el espacio disponible
+            .padding(16.dp) // Añade espacio alrededor del contenido
+    ) {
+        Text(text = "TODOS LOS EVENTOS", modifier = Modifier.padding(vertical = 8.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxWidth()
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize() // Para que ocupe todo el espacio disponible
         ) {
-            ConcertCard(
-                imageId = R.drawable.montaner,
-                artistName = "Ricardo Montaner"
-            )
-            Spacer(modifier = Modifier.width(16.dp))
-            ConcertCard(
-                imageId = R.drawable.wos,
-                artistName = "Wos"
-            )
+            items(4) { index ->
+                val imageId = when (index) {
+                    0 -> R.drawable.wos
+                    1 -> R.drawable.molotov
+                    2 -> R.drawable.montaner
+                    else -> R.drawable.wos
+                }
+                val name = when (index) {
+                    0 -> "Wos"
+                    1 -> "Molotov"
+                    2 -> "Ricardo Montaner"
+                    else -> "Artista desconocido"
+                }
+                ConcertCard(imageId = imageId, artistName = name, place = "Majadas")
+            }
         }
     }
 }
 
 @Composable
-fun ConcertCard(imageId: Int, artistName: String) {
+fun ConcertCard(imageId: Int, artistName: String, place: String) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 8.dp),
+            .padding(vertical = 8.dp)
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
+        Column(
             modifier = Modifier.padding(16.dp)
         ) {
             val concertImage: Painter = painterResource(id = imageId)
             Image(painter = concertImage, contentDescription = "Concierto")
 
-            Spacer(modifier = Modifier.width(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-            ) {
-                Text(
-                    text = artistName,
-                    fontWeight = FontWeight.Bold,
-                    style = MaterialTheme.typography.bodyLarge
-                )
-                Text(text = "Concierto")
-            }
+            Text(
+                text = artistName,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+
+            Text(
+                text = place,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
         }
     }
 }
+
 
 
 
